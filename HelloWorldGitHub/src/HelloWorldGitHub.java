e61324a2d4b267a86a0d59c6957677c1aec369ba
Devon_mac_win7 <loudywen@gmail.com>
2015-01-26 12:25:36 -0500

    33333

--- HelloWorldGitHub/src/HelloWorldGitHub.java
+++ HelloWorldGitHub/src/HelloWorldGitHub.java
@@ -4,7 +4,7 @@ public class HelloWorldGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github");
-		System.out.println("Hello Githu2");
+		System.out.println("Hello Githu3");
 
 	}
 
